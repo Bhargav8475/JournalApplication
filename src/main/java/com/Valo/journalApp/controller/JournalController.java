5c467be07c3aa4a5a4f66620cf96274aeaa773eb
bhargav <bhargav@dataskate.com>
2025-08-22 14:47:35 +0530

    first commmit

---
+++ src/main/java/com/Valo/journalApp/controller/JournalController.java
@@ -0,0 +1,55 @@
+package com.Valo.journalApp.controller;
+
+import com.Valo.journalApp.entity.Journal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/journal")
+class JournalController {
+    private ArrayList<Journal> journalEntries = new ArrayList<Journal>();
+
+    @GetMapping
+    public ArrayList<Journal> getJournalEntries(){
+        return journalEntries;
+    }
+
+    @PostMapping
+    public String createJournalEntry(@RequestBody Journal myJournal){
+        journalEntries.add(myJournal);
+        return "201 Created";
+    }
+
+    @GetMapping("/{myId}")
+    public Journal getJournalById(@PathVariable int myId){
+        for(Journal journal : journalEntries){
+            if(journal.getId() == myId)
+                    return journal;
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{myId}")
+    public String deleteJournalById(@PathVariable int myId){
+        for(int i=0;i< journalEntries.size();i++){
+            if(journalEntries.get(i).getId() == myId){
+                journalEntries.remove(i);
+                return "Successfully deleted";
+            }
+        }
+        return "Journal not found";
+    }
+
+    @PutMapping("/{myId}")
+    public String updateJournal(@PathVariable int myId, @RequestBody Journal updatedJournal) {
+        for (int i = 0; i < journalEntries.size(); i++) {
+            if (journalEntries.get(i).getId() == myId) {
+                journalEntries.set(i, updatedJournal); // replace the old entry with new one
+                return "Updated Successfully";
+            }
+        }
+        return "Journal not found";
+    }
+
+}
